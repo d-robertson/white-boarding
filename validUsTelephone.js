51cfd2f5c01a4da48571cb051e8f0856571e3bd8
derek_robertson <dwilliamrobertson@gmail.com>
2017-01-27 15:20:15 -0800

    phone number validation regex

--- validUsTelephone.js
+++ validUsTelephone.js
@@ -11,3 +11,44 @@
 // For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, you must confirm that the country code is 1. Return true if the string is a valid US phone number; otherwise return false.
 
 
+
+function telephoneCheck(str) {
+  // Good luck!
+  var nonInt = /^[(]?\s?\d/;
+  var paren = /[()]/;
+  var reg1 = /\d?[-]?\s?[(]?\d{3}[)]?[-]?\s?\d{3}\s?[-]?\d{4}/;
+  
+  if(!nonInt.test(str)){
+    return false;
+  }
+  
+  var first = /^[1]/;
+//   console.log(first.test(str), str);
+  
+  if(first.test(str)){
+//     var regWithParen = /\d?[-]?\s?[(]\d{3}[)][-]?\s?[-]?\d{4}/;
+    var regWithParen1 = /[1]\s?[(]\d{3}[)][-]?\s?\d{3}[-]?\s?\d{4}/;
+//     console.log(paren.test(str), str);
+    if(paren.test(str)){
+//       console.log(regWithParen1.test(str), str);
+      return regWithParen1.test(str);
+    }
+//     console.log(reg1.test(str), str);
+    return reg1.test(str);
+  } else {
+    console.log(paren.test(str), str);
+    if(paren.test(str)){
+      var regNo1Paren = /^[(]\d{3}[)][-]?\s?\d{3}[-]?\s?\d{4}$/;
+      return regNo1Paren.test(str);
+    } else {
+      var regNoParenNo1 = /^\d{3}[-]?\s?\d{3}[-]?\s?\d{4}$/;
+      return regNoParenNo1.test(str);
+    }
+  }
+//   console.log(reg1.test(str), 'str: ', str);
+  return reg1.test(str);
+}
+
+
+
+telephoneCheck("555-555-5555");
